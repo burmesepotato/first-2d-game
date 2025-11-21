2c4e9ca8541ebc4065cbef155d1a77e6053f2125
BurmesePotato <burmesepotato.build@proton.me>
2025-11-21 18:30:07 +0700

    Connect timers. Calculate mob spawn location

--- scripts/main.gd
+++ scripts/main.gd
@@ -13,3 +13,22 @@ func new_game() -> void:
 	$Player.start($StartPosition.position)
 	$ScoreTimer.start()
 	
+# Gives Player a few second, then actually starts the game
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
+func _on_score_timer_timeout() -> void:
+	score += 1
+	
+func _on_mob_timer_timeout() -> void:
+	# Creates a mob
+	var mob = mob_scene.instantiate()
+	
+	# MobPath is Path2D node. MobSpawnLocation is PathFollow2D node.
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	
+	# Choose a random location on the path to spawn mob
+	mob_spawn_location.progress_ratio = randf()
+	mob.position = mob_spawn_location.position
+	
